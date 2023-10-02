3c4eef14f76dec379eae3f9507e1c43bb1a2ff5d
Marcos Pego de Sousa <marcospegodesousa10@gmail.com>
2023-10-02 11:35:41 -0300

    hooks

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -4,31 +4,48 @@ import { ContainerLogin, Imagelog } from '../styles/login.styles';
 import Input from '../../../shared/components/input/input';
 import Button from '../../../shared/components/button/Button';
 import { theme } from '../../../shared/themes/theme';
+import { useLogin } from '../hooks/useLogin';
+
 const Login = () => {
-  const handleOnPress = () => {
-    console.log('clicou');
-  };
+  const { email,
+    password,
+    loading,
+    errorMessage,
+    handleOnPress,
+    handleOnChangeEmail,
+    handleOnChangePassword,
+  } = useLogin();
+
   return (
     <View>
       <ContainerLogin>
-        <Imagelog resizeMode="center" source={require("../../../assets/images/download.png")}/>
-        <Input 
-        margin='0px 0px 8px 0px'
-        title='Email:' 
-        placeholder='Digite seu email'
+        <Imagelog
+          resizeMode="center"
+          source={require('../../../assets/images/download.png')}
         />
-        <Input 
-        secureTextEntry
-        title='Senha:' 
-        placeholder='Digite sua senha'
+        <Input
+          value={email}
+          errorMessage={errorMessage}
+          margin="0px 0px 8px 0px"
+          title="Email:"
+          placeholder="Digite seu email"
+          onChange={handleOnChangeEmail}
+        />
+        <Input
+          errorMessage={errorMessage}
+          value={password}
+          secureTextEntry
+          title="Senha:"
+          onChange={handleOnChangePassword}
+          placeholder="Digite sua senha"
         />
         <Button
-          
+          loading={loading}
           type={theme.buttons.buttonsTheme.primary}
           margin="16px"
           title="ENTRAR"
-          
           onPress={handleOnPress}
+
         />
       </ContainerLogin>
     </View>
