81c3451c7bf36a33b7694b42e4a22f6d10752c97
Marcos Pego de Sousa <marcospegodesousa10@gmail.com>
2023-10-03 10:07:39 -0300

    hooks

--- src/shared/hooks/useRequest.ts
+++ src/shared/hooks/useRequest.ts
@@ -3,16 +3,16 @@ import {useState} from 'react';
 import {RequestLogin} from '../types/requestLogin';
 import { connectionAPIPost } from '../functions/connection/connectionAPI';
 import { ReturnLogin } from '../types/returnLogin';
-import { useDispatch } from 'react-redux';
-import { setUserAction } from '../../story/reducers/userReducer';
+import { useUserReducer } from '../../story/reducers/userReducer/useUserReducer';
 export const useRequest = () => {
+    const {setUser} = useUserReducer();
   const [loading, setLoading] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string>('');
-const dispatch = useDispatch();
+
   const authRequest = async (body: RequestLogin) => {
     setLoading(true);
     await connectionAPIPost<ReturnLogin>('http://192.168.2.181:8080/auth', body).then((result)=>{
-        dispatch(setUserAction(result.user));
+        setUser(result.user);
     }).catch(() => {
 
       setErrorMessage('Usuario ou senha invalidos');
