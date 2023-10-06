f203a3a5604d0ebb87778ed95391b2b1bf2be058
Marcos Pego de Sousa <marcospegodesousa10@gmail.com>
2023-10-06 12:17:36 -0300

    navigator icones rodape

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable prettier/prettier *//* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable prettier/prettier */
 
 import Login from './modules/login';
 import { Provider } from 'react-redux';
@@ -10,7 +10,44 @@ import Home from './modules/home';
 import { MenuUrl } from './shared/enums/MenuUrl.wnum';
 import Splash from './modules/splash';
 import CreateUser from './modules/createUser';
+
+import React = require('react');
+import { Icon } from './shared/components/icon/Icon';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { theme } from './shared/themes/theme';
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigation = () => {
+  
+  return (
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({  color, size }) => {
+            let iconName: string;
+            switch (route.name) {
+              case 'Home':
+                iconName = 'home';
+                break;
+              case 'Orders':
+                iconName = 'cart';
+                break;
+              default:
+                iconName = 'profile';
+                break;
+            }
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: theme.colors.mainTheme.primary,
+          tabBarInactiveTintColor: theme.colors.grayTheme.gray80,
+        })}
+      >
+        <Tab.Screen name="Home" component={Home} options={{headerShown: false}}/>
+        <Tab.Screen name="Orders" component={Home} options={{title: 'Pedidos', headerShown: false}}/>
+        <Tab.Screen name="Settings" component={Home} options={{title: 'Perfil', headerShown: false}}/>
+      </Tab.Navigator>
+  );
+};
 const App = () => {
 
   return (
@@ -20,7 +57,7 @@ const App = () => {
         <Stack.Navigator>
         <Stack.Screen name={MenuUrl.SPLASH} component={Splash} options={{headerShown: false}}/>
         <Stack.Screen name={MenuUrl.LOGIN} component={Login} options={{headerShown: false}}/>
-        <Stack.Screen name={MenuUrl.HOME} component={Home} options={{title: 'Home'}}/>
+        <Stack.Screen name={MenuUrl.HOME} component={TabNavigation} options={{headerShown: false}}/>
         <Stack.Screen name={MenuUrl.CREATE_USER} component={CreateUser} options={{title: 'Criar Usuario'}}/>
         </Stack.Navigator>
         </NavigationContainer>
